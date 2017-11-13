b95ba5d625dd3666f49c83242c1955155a51b642
822LL <2851729134@qq.com>
2017-11-13 15:14:10 +0800

    常量，int，string，boolean。 万物皆对象

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RubyDemo
 Ruby学习存档
+
+1.数值精度问题   
+http://www.iteye.com/topic/1143459
--- variable.rb
+++ variable.rb
@@ -1,15 +1,49 @@
-=begin
+#常量
 
-常量
-
-=end
-
-
-puts '===================Number============================';
+puts '===================Number==================';
 
 a = 0
 puts a.methods
 
 b, c = 0.4, 0.3
 d = b - c
+# 看readme 精度问题
 p d
+
+p 1
+
+# toString
+p 1.to_s
+
+puts '===================String==================';
+
+# 拼接,尽量不用 +
+
+puts "hello #{ a + b }"
+
+# puts "hello".methods
+
+
+puts 10 >= 1
+puts "10".eql?("1")
+
+
+puts '===================Range==================';
+
+r = 1..3
+r2 = 1..3
+
+r.each{|x| p x}
+r2.each{|x| p x}
+
+
+arr = [ 1, 2 ,4 ,5]
+
+arr.each{|x| p x}
+
+
+puts '===================Boolean==================';
+
+p true.methods
+
+p "1".empty?
